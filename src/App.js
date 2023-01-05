10143cbb28153d6c37c9e88757cab467e82afd5c
mahadi608 <mdmahadi608@gmail.com>
2023-01-06 00:15:16 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
-    </div>
-  );
+  function formatName(username) {
+    return username.toUpperCase();
+  }
+  function getGreetin(name) {
+    if (name) {
+      return <h1>Hello {formatName(name)}</h1>;
+    } else {
+      return <h1>Hello Friend</h1>;
+    }
+  }
+  const userName = 'emran';
+  //const element = <h1>Hello {formatName(userName)}</h1>;
+  //const image = <img src="./logo192.png" alt="logo" />;
+
+  return getGreetin(userName);
 }
 
 export default App;
